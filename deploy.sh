76917aacfb0fd3ee2ff6aa5f182e323edd5a7dcd
hbao <903064667@qq.com>
2017-10-18 01:45:16 -0500

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
-
 cd /Users/hbao/Downloads/apache-tomcat-7.0.82/bin
 ./shutdown.sh
-rm -rf /Users/hbao/Downloads/apache-tomcat-7.0.82/webapps
+rm -rf /Users/hbao/Downloads/apache-tomcat-7.0.82/webapps/webdemo
 rm -f /Users/hbao/Downloads/apache-tomcat-7.0.82/webapps/webdemo.war
 cd /var/jenkins_home/workspace/TestForPipeline/webdemo/build/libs
 scp webdemo.war hbao@10.209.21.215:/Users/hbao/Downloads/apache-tomcat-7.0.82/
