61ba5119ed1b4204089eb817cf59ac689fe3648f
Mokaffe <903064667@qq.com>
2017-10-18 18:51:04 +0800

    sss

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@
 set -e
 ssh hbao@10.209.21.215 '
 tomcat_path = /Users/hbao/Downloads/apache-tomcat-7.0.82
-TomcatID = $(ps -ef |grep tomcat |grep -w $tomcat_path|grep -v 'grep'|awk '{print $2}')
+TomcatID=$(ps -ef |grep tomcat |grep -w $tomcat_path|grep -v 'grep'|awk '{print $2}')
                 
 StopTomcat = /Users/hbao/Downloads/apache-tomcat-7.0.82/bin/shutdown.sh
                 
