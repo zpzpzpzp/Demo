138e6d6020df271a8ad6b8edf7d731d88bd72d15
hbao <903064667@qq.com>
2017-10-18 02:44:36 -0500

    tired

--- deploy.sh
+++ deploy.sh
@@ -7,7 +7,4 @@ rm -f /Users/hbao/Downloads/apache-tomcat-7.0.82/webapps/webdemo.war
 exit
 cd /var/jenkins_home/workspace/TestForPipeline/webdemo/build/libs
 scp webdemo.war hbao@10.209.21.215:/Users/hbao/Downloads/apache-tomcat-7.0.82/webapps
-ssh -tt hbao@10.209.21.215
-cd /Users/hbao/Downloads/apache-tomcat-7.0.82/bin
-./startup.sh
-exit
+
