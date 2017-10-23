7616ef3d5b870fdf2ea64e48955d60a0d2d0eb39
hbao <903064667@qq.com>
2017-10-23 02:02:53 -0500

    Update HelloServlet.java

--- webdemo/src/main/java/org/gradle/demo/HelloServlet.java
+++ webdemo/src/main/java/org/gradle/demo/HelloServlet.java
@@ -18,9 +18,9 @@ public class HelloServlet extends HttpServlet {
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        String name = request.getParameter("name..");
+        String name = request.getParameter("name");
         if (name == null) name = "World";
-        request.setAttribute("user", name);
-        request.getRequestDispatcher("response.jsp").forward(request, response);
+        //request.setAttribute("user", name);
+        //request.getRequestDispatcher("response.jsp").forward(request, response);
     }
 }
