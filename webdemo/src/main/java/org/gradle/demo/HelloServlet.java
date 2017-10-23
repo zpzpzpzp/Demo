1d2797972cafd2112e954c06458edaa4464293da
hbao <903064667@qq.com>
2017-10-23 01:04:28 -0500

    ..name

--- webdemo/src/main/java/org/gradle/demo/HelloServlet.java
+++ webdemo/src/main/java/org/gradle/demo/HelloServlet.java
@@ -18,7 +18,7 @@ public class HelloServlet extends HttpServlet {
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        String name = request.getParameter("name");
+        String name = request.getParameter("name..");
         if (name == null) name = "World";
         request.setAttribute("user", name);
         request.getRequestDispatcher("response.jsp").forward(request, response);
